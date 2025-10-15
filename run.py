127954aeb6999dfa82306ffe5bdd5a11872f433a
Flamefusion <meshekharbehera613@gmail.com>
2025-10-15 16:50:23 +0530

    backend: migrated to waitress
    we arre going production ready , from default flask development server to waitress

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+import logging
 from app import create_app
+from waitress import serve
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Create Flask app instance
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    serve(app, host='0.0.0.0', port=5000)
